0b009224ca533cbe859bee3a3268cc28dec86f59
なりたけいすけ <narita_keisuke@cyberagent.co.jp>
2017-09-03 01:59:49 +0900

    Initial Commit

---
+++ hawkeye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hawkeye
+//
+//  Created by 成田 圭介 on 2017/09/03.
+//  Copyright © 2017年 ibot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
